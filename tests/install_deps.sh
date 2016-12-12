b70d47ac1077e6036294ac2df91539e4aa8b4389
Brenda Wallace <brenda@wallace.net.nz>
2016-12-12 17:01:00 +1300

    Update DHT Sensor library to 1.3.0

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -6,4 +6,4 @@ DEPENDENCY_OUTPUT=$(arduino --install-library USBHost > /dev/null 2>&1)
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 arduino --install-library "MySensors:2.0.0"
-arduino --install-library "DHT sensor library:1.2.3"
+arduino --install-library "DHT sensor library:1.3.0"
